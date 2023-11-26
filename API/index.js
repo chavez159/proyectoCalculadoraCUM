43a186a72527fd30006b719949891cc9b3de59f7
nicolas2321-ux <00035819@uca.edu.sv>
2023-11-25 23:10:21 -0600

    add: mongo atlas

--- API/index.js
+++ API/index.js
@@ -5,7 +5,7 @@ const main = () => {
 
     app.listen(app.get("port"))
     console.log(`server on port ${app.get("port")}`)
-    mongoose.connect('mongodb://localhost:27017/proyecto');
+    mongoose.connect('mongodb+srv://orellanaj2321:palodecoco2321@api.mlsey5j.mongodb.net/?retryWrites=true');
     const db =  mongoose.connection;
     db.on('error', console.error.bind(console, 'Error de conexión a MongoDB:'));
     db.once('open', () => {
