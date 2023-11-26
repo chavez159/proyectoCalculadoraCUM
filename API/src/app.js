e238873916fabea179c0baee725fa0a457283c74
nicolas2321-ux <00035819@uca.edu.sv>
2023-11-25 22:50:43 -0600

    finish api

--- API/src/app.js
+++ API/src/app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import cors from "cors";
 import bodyParser  from 'body-parser'
 import userRoutes from "./routes/userRoutes";
+import noticiasRoutes from "./routes/noticiasRoutes";
+import faqRoutes from "./routes/FaqRoutes";
 import mongoose from "mongoose";
 const app = express();
 
@@ -19,5 +21,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.json())
 app.use('/api/users', userRoutes);
+app.use('/api/noticias', noticiasRoutes);
+app.use('/api/faq', faqRoutes);
 
 export default app;
